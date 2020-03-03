1bdd1567dc4da91a3c9be328ae49cf810362bc18
Emrah <komnatadeveloper@gmail.com>
2020-03-03 23:56:25 +0300

    WE are developing our State Management

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+import './providers/products_provider.dart';
+
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) 
+  {
+    return ChangeNotifierProvider(
+
+      builder: ( _ ) => ProductsProvider(),
+
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato'
+        ),
+        home: ProductsOverviewScreen(),
+
+        routes: {
+          ProductDetailScreen.routeName : (ctx) => ProductDetailScreen()
+        },
+      ),
+    );
+  }
+}
+
+
