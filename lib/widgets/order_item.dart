fd532df7db79ac7184145beadbecb80ef564773b
komnata <ozyazgane@hotmail.com>
2020-04-09 19:42:22 +0300

    Added animation to Order Item

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -14,70 +14,89 @@ class OrderItem extends StatefulWidget {
   _OrderItemState createState() => _OrderItemState();
 }
 
-class _OrderItemState extends State<OrderItem> {
+
+// STATE
+class _OrderItemState extends State<OrderItem> with SingleTickerProviderStateMixin {
 
   var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text(
-              '\$${widget.order.amount.toStringAsFixed(2)}'
-            ),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)
-            ),
-            trailing: IconButton(
-              icon: Icon( _expanded 
-                ? Icons.expand_less
-                : Icons.expand_more
+    return AnimatedContainer(
+      height: _expanded 
+      ? min(
+        widget.order.products.length * 25.0 + 110,
+        200
+      ) 
+      : 95,
+      duration: Duration(milliseconds: 300),
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                '\$${widget.order.amount.toStringAsFixed(2)}'
               ),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)
+              ),
+              trailing: IconButton(
+                icon: Icon( _expanded 
+                  ? Icons.expand_less
+                  : Icons.expand_more
+                ),
+                onPressed: () {
+                  setState(() {
+                    // if( !_expanded  ) {
+                    //   _controller.forward();
+                    // } else {
+                    //   _controller.reverse();
+                    // }
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-          ),
 
-          // if Expanded
-          if( _expanded) Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 15,
-              vertical: 4
-            ),
-            height: min(
-              widget.order.products.length * 25.0 + 10,
-              130
-            ),
-            child: ListView.builder(
-              itemCount: widget.order.products.length,
-              itemBuilder: ( _ , index ) => Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    widget.order.products[index].title,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  Text(
-                    '${widget.order.products[index].quantity}x \$${widget.order.products[index].price}',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey
-                    ),
-                  )
-                ],
+            // if Expanded
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4
+              ),
+              height: _expanded
+              ? min(
+                widget.order.products.length * 25.0 + 10,
+                130
               )
-            ),
-          )
-        ],
+              : 0,
+              child: ListView.builder(
+                itemCount: widget.order.products.length,
+                itemBuilder: ( _ , index ) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      widget.order.products[index].title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      '${widget.order.products[index].quantity}x \$${widget.order.products[index].price}',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey
+                      ),
+                    )
+                  ],
+                )
+              ),
+            ),            
+          ],
+        ),
       ),
     );
   }
